75ae1041e98a6fecd3f5a2c0974469889d44338b
antonio krijan <antonio.krijan0218@gmail.com>
2023-05-14 20:32:12 +0100

    medisure

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCC9vqQ190__WtC6l46t5sMLvVhInwnRNM",
+  authDomain: "mytestproject-e2282.firebaseapp.com",
+  projectId: "mytestproject-e2282",
+  storageBucket: "mytestproject-e2282.appspot.com",
+  messagingSenderId: "743500671021",
+  appId: "1:743500671021:web:982792ad4f3042fcca30a4",
+};
+
+export default firebaseConfig;
